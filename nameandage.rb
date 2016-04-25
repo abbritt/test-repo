d1853526dd35b465a8d84003f4e8ecc3735bdce7
abbritt <saunders@actblue.com>
2016-04-25 15:46:58 -0400

    woo
    we’re doing it live!

---
+++ nameandage.rb
@@ -0,0 +1,8 @@
+#Write a program that asks for a name and an age. Have it output something like “Bob was born in 1985”
+
+#get user name
+print "What's your name?"
+name = gets.chomp
+print "What's your age?"
+age = gets.chomp 
+print "Your name is #{name} and you are #{age} years old."
