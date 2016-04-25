cb024e2adcf056f1a4ffc3eb7b0adec904ea51f6
abbritt <saunders@actblue.com>
2016-04-25 16:01:20 -0400

    finished homework

---
+++ madlibs.rb
@@ -0,0 +1,17 @@
+#create some mad libs, yo
+
+#what are we gonna mad lib?
+
+print "What's your favorite verb in the past tense?"
+verb = gets.chomp
+print "What's your favorite adjective?"
+adj = gets.chomp
+print "What's something a dog does?"
+dog = gets.chomp
+print "Name an exotic animal"
+animal = gets.chomp
+
+puts "Just #{verb} up in the mornin gotta thank God"
+puts "I don't know, but today seems kinda #{adj}" 
+puts "No #{dog} from the dog, no smog"
+puts "And momma cooked a breakfast with no #{animal} (damn)"
